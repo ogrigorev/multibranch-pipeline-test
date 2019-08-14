817bbf2a6726e90d434fcc662ecda482c7c50475
Oleg Grigorev <Oleg.Grigorev@grassvalley.com>
2019-08-14 15:23:49 -0400

    error fix

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
             assertEquals("Hello World! This is a multiBranch PipiLine Test!!!!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"Hello World! This is a multiBranch PipiLine Test!!!!\"");
         }
     }
 
